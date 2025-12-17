9389b800906814ba8ec682e0918077333f720872
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2025-12-17 16:41:14 +0100

    Finalización.

--- 2025-2026 DAM 2C DI Devolver Informacion/SegundaViewController.swift
+++ 2025-2026 DAM 2C DI Devolver Informacion/SegundaViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class SegundaViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtTexto: UITextField!
+    
+    var delegate: ObtenerDatosProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnVolver(_ sender: Any) {
+        let texto = txtTexto.text!
+        
+        delegate?.obtener(dato: texto)
+        
+        self.dismiss(animated: true)
     }
-    */
-
 }
--- 2025-2026 DAM 2C DI Devolver Informacion/ViewController.swift
+++ 2025-2026 DAM 2C DI Devolver Informacion/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ObtenerDatosProtocol {
+    
+    func obtener(dato: String) {
+        lblTexto.text = dato
+    }
+    
+    
+    @IBOutlet weak var lblTexto: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPasar(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "PASAR") {
+            let destino = segue.destination as! SegundaViewController
+            destino.delegate = self
+        }
+    }
 }
-
